3051498ab1891db18583590acd9b45e521171639
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2021-10-03 19:11:06 +0300

    css değişiklikleri

--- PhoneList/src/components/Contacts/List/index.js
+++ PhoneList/src/components/Contacts/List/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import ".././styles.css";
 
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key].toString().toLowerCase().includes(filterText.toLowerCase())
+    );
+  });
+  console.log("filter", filtered);
+
   return (
     <div>
-      Contects List
-      <ul>
-        {contacts.map((contact, index) => (
-          <li key={index}>{contact.fullname}</li>
+      <h1>Contacts</h1>
+      <br />
+      <input
+        placeholder="Filter Contacts"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
+      <ul className="list">
+        {filtered.map((contact, index) => (
+          <li key={index}>
+            <span>{contact.fullname}</span>
+            <span>{contact.phone_number}</span>
+          </li>
         ))}
       </ul>
+      <p>Total Contacts ({filtered.length})</p>
     </div>
   );
 }
--- PhoneList/src/components/Contacts/index.js
+++ PhoneList/src/components/Contacts/index.js
@@ -1,14 +1,32 @@
 import { useState, useEffect } from "react";
 import List from "./List";
 import Form from "./Form";
+import "./styles.css";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Mehmet",
+      phone_number: 123123,
+    },
+    {
+      fullname: "Ahmet",
+      phone_number: 42564257,
+    },
+    {
+      fullname: "Burcu",
+      phone_number: 41256,
+    },
+    {
+      fullname: "Merve",
+      phone_number: 567845,
+    },
+  ]);
   useEffect(() => {
     console.log(contacts);
   }, [contacts]);
   return (
-    <div>
+    <div id="container">
       <List contacts={contacts} />
       <Form addContact={setContacts} contacts={contacts} />
     </div>
