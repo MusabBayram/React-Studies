7a1b3f544b224c5ddac52449aa230244ac44f1e9
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2021-10-03 19:32:16 +0300

    HW2

---
+++ ReactHW2/src/App.js
@@ -0,0 +1,80 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <form>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autofocus
+            />
+          </form>
+        </header>
+        {/* This section should be hidden by default and shown when there are todos */}
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn JavaScript</label>
+                <button className="destroy" />
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn React</label>
+                <button className="destroy" />
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Have a life!</label>
+                <button className="destroy" />
+              </div>
+            </li>
+          </ul>
+        </section>
+        {/* This footer should hidden by default and shown when there are todos */}
+        <footer className="footer">
+          {/* This should be `0 items left` by default */}
+          <span className="todo-count">
+            <strong>2</strong>
+            items left
+          </span>
+          <ul className="filters">
+            <li>
+              <a className="selected">All</a>
+            </li>
+            <li>
+              <a>Active</a>
+            </li>
+            <li>
+              <a>Completed</a>
+            </li>
+          </ul>
+          {/* Hidden if no completed items are left ↓ */}
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+      <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>
+          Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
+        </p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
